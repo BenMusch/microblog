0454cfaaf57ca32a4678f9502252d19ea087cfab
Ben Muschol <benmuschol@gmail.com>
2017-10-16 17:53:27 -0400

    Move message rendering to Vue (#10)

---
+++ assets/js/components/message-list.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="message-list">
+    <div v-for="message in messages" class="row message item" :id="'message-' + message.id">
+      <div class="col-md-1">
+        <div class="avatar-container-sm panel">
+          <img :src="message.avatarUrl" alt="avatar"/>
+        </div>
+      </div>
+
+      <div class="col-md-11 content">
+        <p>{{ message.content }}</p>
+
+        <like-btn
+           :user-id="currentUserId"
+           :message-id="message.id"
+           :liked="message.likedByCurrentUser"
+           :count="message.numLikes"
+           target-url="/api/likes"
+        />
+
+        <span class="metadata">
+          <strong>
+            <a :href="message.user.path" class="text-dark">{{ message.user.name }}</a>
+          </strong>
+          <a :href="message.user.path" class="text-secondary">@{{ message.user.handle }}</a>
+
+          <small>
+            <a :href="message.path">{{ message.createdAt }}</a>
+            <a data-confirm="Are you sure?" :data-csrf="csrfToken"
+              data-method="delete" :data-to="message.path" href="#"
+              rel="nofollow" class="text-danger"
+              v-if="message.userId == currentUserId">Delete</a>
+          </small>
+        </span>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Like from "./like.vue"
+export default {
+  name: 'message-list',
+  props: ['initialData', 'currentUserId', 'csrfToken'],
+  components: {
+    'like-btn': Like
+  },
+  data() {
+    return {
+      messages: this.initialData
+    }
+  }
+}
+</script>
--- lib/microblog_web/views/message_view.ex
+++ lib/microblog_web/views/message_view.ex
@@ -1,3 +1,30 @@
 defmodule MicroblogWeb.MessageView do
   use MicroblogWeb, :view
+
+  def message_json(conn, message, current_user \\ nil) do
+    url = message_path(conn, :delete, message)
+    user_url = user_path(conn, :show, 1)
+    %{
+      "user" => %{
+        "id" => message.user.id,
+        "path" => user_url,
+        "handle" => message.user.handle,
+        "name" => message.user.human_name
+      },
+      "id" => message.id,
+      "userId" => message.user_id,
+      "createdAt" => "#{message.inserted_at}",
+      "likedByCurrentUser" => current_user && Microblog.Social.user_likes?(current_user, message),
+      "numLikes" => Microblog.Social.num_likes(message),
+      "avatarUrl" => Gravity.image(message.user.email),
+      "content" => message.content,
+      "path" => url
+    }
+  end
+
+  def encode_messages(conn, messages, current_user \\ nil) do
+    messages = Enum.map(messages, fn m -> message_json(conn, m, current_user) end)
+    encoded = Poison.encode!(messages)
+    encoded
+  end
 end
