bdd91ffbfc6af590cffb388fe70f8f150884c889
Ben Muschol <benmuschol@gmail.com>
2017-10-16 13:01:40 -0400

    wip

---
+++ assets/js/components/message-list.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="message-list">
+    <div v-for="message in messages" class="row message item" :id="'message-' + message.id">
+      <div class="col-md-1">
+        <div class="avatar-container-sm panel">
+          <img :src="avatarUrl" alt="avatar"/>
+        </div>
+      </div>
+
+      <div class="col-md-11 content">
+        <p>{{ message.content }}</p>
+
+        <like-btn
+           :user-id="userId"
+           :message-id="message.id"
+           :liked="message.likedByCurrentUser"
+           :count="message.numLikes"
+           target-url="/api/likes"
+          />
+      </div>
+    </div>
+  </div>
+</template>
